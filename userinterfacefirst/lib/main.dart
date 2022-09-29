13869d8ab005011ad0f9b1e0c9461493a1879c63
zeeshi045 <zeeshanfarooq486@gmail.com>
2022-09-29 16:33:24 +0500

    autotext plus icon add
    pubget autotext servies plus icon add

--- userinterfacefirst/lib/main.dart
+++ userinterfacefirst/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:auto_size_text/auto_size_text.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -20,24 +20,22 @@ class MyApp extends StatelessWidget {
          children: [
            SizedBox(width: 30,
            height: 80,),
-        Text('Hi, Den!',
+           AutoSizeText('Hi, Den!',
           style:TextStyle(color: Colors.black,
             fontSize: 18,
           ),
         ),
+           Spacer(),
+           Icon(
+             Icons.notifications,
+             color:Colors.black87,
+             size: 19.0,
+           ),
+           SizedBox(width: 34),
          ],
           ),
-          Column(
-            children: [
-      Text('Hi, Den!',
-        style:TextStyle(color: Colors.black,
-          fontSize: 18,
-        ),
-      ),
-           ] ,
-          ),
-
   ],
+
         ),
         ),
     );
