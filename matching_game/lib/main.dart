ab46cdeef9ebc5a72e600f7d4e1267ec096dd886
zeeshi045 <zeeshanfarooq486@gmail.com>
2022-10-12 11:53:29 +0500

    design lvl 1
    design draw kiya han

--- matching_game/lib/main.dart
+++ matching_game/lib/main.dart
@@ -50,11 +50,12 @@ class SecondScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Splash Screen',
         theme: ThemeData(
         primarySwatch: Colors.green,
     ),
     home: home(),
+      debugShowCheckedModeBanner: false,
+
     );
   }
 }
@@ -71,6 +72,139 @@ class _homeState extends State<home> {
     return Scaffold(
       appBar: AppBar(
         title:Text('Matching game'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children:[
+            Text('Level 1',
+              style: TextStyle(
+                fontSize:30,
+                color: Colors.black87,
+              ),
+            ),
+          SizedBox(height: 10,),
+            Row(
+              children: [
+                TextButton(
+                  onPressed:(){},
+                  child: Image.asset("images/2.JPG",
+                    height: 60,
+                    width: 66,
+                  ),
+                ),
+                Spacer(),
+                TextButton(
+                  onPressed:(){},
+                  child: Text('4',
+                    style: TextStyle(
+                      fontSize:22,
+                      color: Colors.amber,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+
+            ),
+            SizedBox(height: 15,),
+            Row(
+              children: [
+                TextButton(
+                  onPressed:(){},
+                  child: Image.asset("images/4.JPG",
+                    height: 60,
+                    width: 66,
+                  ),
+                ),
+                Spacer(),
+                TextButton(
+                  onPressed:(){},
+                  child: Text('3',
+                    style: TextStyle(
+                      fontSize:22,
+                      color: Colors.red,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 15,),
+            Row(
+              children: [
+                TextButton(
+                  onPressed:(){},
+                  child: Image.asset("images/5.png",
+                    height: 60,
+                    width: 66,
+                  ),
+                ),
+                Spacer(),
+                TextButton(
+                  onPressed:(){},
+                  child: Text('1',
+                    style: TextStyle(
+                      fontSize:22,
+                      color: Colors.brown,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+
+            ),
+            SizedBox(height: 15,),
+            Row(
+              children: [
+                TextButton(
+                  onPressed:(){},
+                  child: Image.asset("images/1.JPG",
+                    height: 60,
+                    width: 66,
+                  ),
+                ),
+                Spacer(),
+                TextButton(
+                  onPressed:(){},
+                  child: Text('5',
+                    style: TextStyle(
+                      fontSize:22,
+                      color: Colors.black87,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+
+            ),
+            SizedBox(height: 15,),
+            Row(
+              children: [
+                TextButton(
+                  onPressed:(){},
+                  child: Image.asset("images/3.png",
+                    height: 60,
+                    width: 66,
+                  ),
+                ),
+                Spacer(),
+                TextButton(
+                  onPressed:(){},
+                  child: Text('2',
+                    style: TextStyle(
+                      fontSize:22,
+                      color: Colors.purple,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+
+            ),
+          ],
+        ),
       ),
     );
   }
