0d4ec2d5c8ebdeb619583e1f6b33814d1dcc0509
zeeshi045 <zeeshanfarooq486@gmail.com>
2022-10-18 15:40:15 +0500

    logic set
    desgin logic

--- guess_game/lib/main.dart
+++ guess_game/lib/main.dart
@@ -1,115 +1,121 @@
 import 'package:flutter/material.dart';
-
+import 'dart:math';
 void main() {
-  runApp(const MyApp());
+  runApp(const guess_game());
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class guess_game extends StatefulWidget {
+  const guess_game({Key? key}) : super(key: key);
   @override
+  State<guess_game> createState() => _guess_gameState();
+}
+class _guess_gameState extends State<guess_game> {
+  @override
+  int x=0;
+  int num1=0;
+  int num2=0;
+  int num3=0;
+  int bt1=0;
+  int bt2=0;
+  int bt3=0;
+  int num4=0;
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return  MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+              title:Text('Guess_Number'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          body: Container(
+              child:Column(
+                children: [
+                  TextButton(
+                    onPressed: (){
+                      Random random = new Random();
+                      num1 = random.nextInt(100) + 1;
+                      print(num1);
+                      Random random2 = new Random();
+                      num2 = random2.nextInt(100) + 1;
+                      print(num2);
+                      Random random3 = new Random();
+                      num3 = random3.nextInt(100) + 1;
+                      print(num3);
+                      Random priority = new Random();
+                      num4 = priority.nextInt(3) + 1;
+                      print(num4);
+                      x=1;
+                      if(num4==1){
+                        setState(() {
+                          bt1=num2;
+                          bt2=num3;
+                          bt3=num1;
+                        });
+
+                      }
+                      if(num4==2){
+                        setState(() {
+
+                          bt1=num1;
+                          bt2=num2;
+                          bt3=num3;
+                        });
+                      }
+                      if(num4==3) {
+                        setState(() {
+                          bt1 = num3;
+                          bt2 = num1;
+                          bt3 = num2;
+                        });
+                      };
+
+                    },
+                    child: Text("genrate random number"),
+                  ),
+
+                  TextButton(
+                    onPressed: () {
+                      if (x == 1) {
+                        if (bt1 == num1) {
+                          print("correct");
+                        }
+                        else
+                          print("wrong");
+                      }
+
+                    },
+                    child: Text("a)      $bt1"),
+                  ),
+                  TextButton(
+                    onPressed: (){
+                      if(x==1) {
+                        if (bt2 == num1) {
+                          print("correct");
+                        }
+                        else
+                          print("wrong");
+                      }
+
+                    },
+                    child: Text("b)      $bt2"),
+                  ),
+                  TextButton(
+                    onPressed: (){
+                      if(x==1) {
+                        if (bt3 == num1) {
+                          print("correct");
+                        }
+                        else
+                          print("wrong");
+                      }
+                    },
+                    child: Text("c)      $bt3"),
+                  ),
+                ],
+              )
+          ),
+        )
     );
+
   }
 }
+
