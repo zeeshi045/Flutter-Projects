25b0d68909f2831bc0fd76862757e6cf316aebc4
zeeshi045 <zeeshanfarooq486@gmail.com>
2022-10-18 16:06:20 +0500

    background chge
    changing in logic plus background gradient clr use

--- guess_game/lib/main.dart
+++ guess_game/lib/main.dart
@@ -18,6 +18,7 @@ class _guess_gameState extends State<guess_game> {
   int bt2=0;
   int bt3=0;
   int num4=0;
+
   Widget build(BuildContext context) {
     return  MaterialApp(
         debugShowCheckedModeBanner: false,
@@ -27,90 +28,105 @@ class _guess_gameState extends State<guess_game> {
               title:Text('Guess_Number'),
             ),
           body: Container(
-              child:Column(
-                children: [
-                  TextButton(
-                    onPressed: (){
-                      Random random = new Random();
-                      num1 = random.nextInt(100) + 1;
-                      print(num1);
-                      Random random2 = new Random();
-                      num2 = random2.nextInt(100) + 1;
-                      print(num2);
-                      Random random3 = new Random();
-                      num3 = random3.nextInt(100) + 1;
-                      print(num3);
-                      Random priority = new Random();
-                      num4 = priority.nextInt(3) + 1;
-                      print(num4);
-                      x=1;
-                      if(num4==1){
-                        setState(() {
-                          bt1=num2;
-                          bt2=num3;
-                          bt3=num1;
-                        });
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.centerRight,
+                    end: Alignment.bottomLeft,
+                    colors: [
+                      Colors.amber,
+                      Colors.black87,
 
-                      }
-                      if(num4==2){
-                        setState(() {
-
-                          bt1=num1;
-                          bt2=num2;
-                          bt3=num3;
-                        });
-                      }
-                      if(num4==3) {
-                        setState(() {
-                          bt1 = num3;
-                          bt2 = num1;
-                          bt3 = num2;
-                        });
-                      };
-
-                    },
-                    child: Text("genrate random number"),
-                  ),
-
-                  TextButton(
-                    onPressed: () {
-                      if (x == 1) {
-                        if (bt1 == num1) {
-                          print("correct");
+                    ],
+                  )
+              ),
+              child:Center(
+                child: Column( mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextButton(
+                      onPressed: (){
+                        Random random = new Random();
+                        num1 = random.nextInt(25) + 1;
+                        print(num1);
+                        Random random2 = new Random();
+                        num2 = random2.nextInt(25) + 1;
+                        print(num2);
+                        Random random3 = new Random();
+                        num3 = random3.nextInt(25) + 1;
+                        print(num3);
+                        Random priority = new Random();
+                        num4 = priority.nextInt(3) + 1;
+                        print(num4);
+                        x=1;
+                        if(num4==1){
+                          setState(() {
+                            bt1=num2;
+                            bt2=num3;
+                            bt3=num1;
+                          });
                         }
-                        else
-                          print("wrong");
-                      }
+                        if(num4==2){
+                          setState(() {
 
-                    },
-                    child: Text("a)      $bt1"),
-                  ),
-                  TextButton(
-                    onPressed: (){
-                      if(x==1) {
-                        if (bt2 == num1) {
-                          print("correct");
+                            bt1=num1;
+                            bt2=num2;
+                            bt3=num3;
+                          });
                         }
-                        else
-                          print("wrong");
-                      }
+                        if(num4==3) {
+                          setState(() {
+                            bt1 = num3;
+                            bt2 = num1;
+                            bt3 = num2;
+                          });
+                        };
 
-                    },
-                    child: Text("b)      $bt2"),
-                  ),
-                  TextButton(
-                    onPressed: (){
-                      if(x==1) {
-                        if (bt3 == num1) {
-                          print("correct");
+                      },
+                      child: Text("genrate random number"),
+                      style: TextButton.styleFrom(
+                        primary: Colors.teal,
+                        shape: const BeveledRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(5))),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        if (x == 1) {
+                          if (bt1 == num1) {
+                            print("correct");
+                          }
+                          else
+                            print("wrong");
                         }
-                        else
-                          print("wrong");
-                      }
-                    },
-                    child: Text("c)      $bt3"),
-                  ),
-                ],
+
+                      },
+                      child: Text("a)      $bt1"),
+                    ),
+                    TextButton(
+                      onPressed: (){
+                        if(x==1) {
+                          if (bt2 == num1) {
+                            print("correct");
+                          }
+                          else
+                            print("wrong");
+                        }
+
+                      },
+                      child: Text("b)      $bt2"),
+                    ),
+                    TextButton(
+                      onPressed: (){
+                        if(x==1) {
+                          if (bt3 == num1) {
+                            print("correct");
+                          }
+                          else
+                            print("wrong");
+                        }
+                      },
+                      child: Text("c)      $bt3"),
+                    ),
+                  ],
+                ),
               )
           ),
         )
