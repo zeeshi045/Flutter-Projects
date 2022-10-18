52b4a9aaa40d6518f70349a4062516656a316e05
zeeshi045 <zeeshanfarooq486@gmail.com>
2022-10-18 23:33:33 +0500

    final
    complete

--- guess_game/lib/main.dart
+++ guess_game/lib/main.dart
@@ -19,185 +19,265 @@ class _guess_gameState extends State<guess_game> {
   int b1=0;
   int b2=0;
   int b3=0;
-  bool clr=false;
-  Color  c=Colors.orangeAccent;
+  Color r=Colors.red;
+  Color w=Colors.white;
+  Color g=Colors.green;
+  Color r2=Colors.red;
+  Color w2=Colors.white;
+  Color g2=Colors.green;
+  Color r3=Colors.red;
+  Color w3=Colors.white;
+  Color g3=Colors.green;
+
   Widget build(BuildContext context) {
     return  MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(
-            centerTitle: true,
-              title:Text('Guess_Number'),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.pink,
+          title:Text('Guess_Number'),
+        ),
+        body: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.centerRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    Colors.amber,
+                    Colors.white,
+                  ],
+                )
             ),
-          body: Container(
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.centerRight,
-                    end: Alignment.bottomLeft,
-                    colors: [
-                      Colors.amber,
-                      Colors.white,
-                    ],
-                  )
-              ),
-              child:Center(
-                child: Column(
-                  children: [
-                    SizedBox(height: 20,),
-                    Text('Range is (1-25)',
+            child:Center(
+              child: Column(
+                children: [
+                  SizedBox(height: 20,),
+                  Text('Range is (1-25)',
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 20,
                       color: Colors.black,
                     ),),
-                    SizedBox(height: 50,),
-                    TextButton(
-                      onPressed: (){
-                        Random random = new Random();
-                        n1 = random.nextInt(25) + 1;
-                        print(n1);
-                        Random random2 = new Random();
-                        n2 = random2.nextInt(25) + 1;
-                        print(n2);
-                        Random random3 = new Random();
-                        n3 = random3.nextInt(25) + 1;
-                        print(n3);
-                        Random priority = new Random();
-                        n4 = priority.nextInt(3) + 1;
-                        print(n4);
-                        x=1;
-                        if(n4==1){
-                          setState(() {
-                            b1=n2;
-                            b2=n3;
-                            b3=n1;
-                          });
-                        }
-                        if(n4==2){
-                          setState(() {
-
-                            b1=n1;
-                            b2=n2;
-                            b3=n3;
-                          });
-                        }
-                        if(n4==3) {
-                          setState(() {
-                            b1 = n3;
-                            b2 = n1;
-                            b3 = n2;
-                          });
-                        };
-                      },
-                      child: Text('Press Me',
+                  SizedBox(height: 50,),
+                  TextButton(
+                    onPressed: (){
+                      Random random = new Random();
+                      n1 = random.nextInt(25) + 1;
+                      print(n1);
+                      Random random2 = new Random();
+                      n2 = random2.nextInt(25) + 1;
+                      print(n2);
+                      Random random3 = new Random();
+                      n3 = random3.nextInt(25) + 1;
+                      print(n3);
+                      Random priority = new Random();
+                      n4 = priority.nextInt(3) + 1;
+                      print(n4);
+                      x=1;
+                      if(n4==1){
+                        setState(() {
+                          b1=n2;
+                          b2=n3;
+                          b3=n1;
+                        });
+                      }
+                      if(n4==2){
+                        setState(() {
+                          b1=n1;
+                          b2=n2;
+                          b3=n3;
+                        });
+                      }
+                      if(n4==3) {
+                        setState(() {
+                          b1 = n3;
+                          b2 = n1;
+                          b3 = n2;
+                        });
+                      };
+                    },
+                    child: Text('Press Me',
                       style: TextStyle(
                         fontSize: 18,
                         fontWeight: FontWeight.bold,
                         color: Colors.black,
                       ),),
-                style: ElevatedButton.styleFrom(
-                    shadowColor: Colors.black,
-                    backgroundColor: Colors.blueAccent,
-                    shape: StadiumBorder(),
-                    padding: EdgeInsets.symmetric(horizontal: 35,vertical: 20)),
-              ),
-                    SizedBox(height: 10,),
-                    Container(
-                      color: Colors.red,
-                      child: TextButton(
-                        onPressed: () {
-                          if (x == 1) {
-                            if (b1 == n1) {
-                              AssetsAudioPlayer.newPlayer().open(
-                                Audio("assets/correct.ogg"),
-                              );
-                              print("correct");
-                            }
-                            else {
-                              AssetsAudioPlayer.newPlayer().open(
-                                Audio("assets/not.ogg"),
-                              );
-                              print("wrong");
-                            }
+                    style: ElevatedButton.styleFrom(
+                        shadowColor: Colors.black,
+                        backgroundColor: Colors.blueAccent,
+                        shape: StadiumBorder(),
+                        padding: EdgeInsets.symmetric(horizontal: 35,vertical: 20)),
+                  ),
+                  SizedBox(height: 10,),
+                  Container(
+                    color: w,
+                    child: TextButton(
+                      onPressed: () {
+                        if (x == 1) {
+                          if (b1 == n1){
+                            AssetsAudioPlayer.newPlayer().open(
+                              Audio("assets/correct.ogg"),
+                            );
+                            setState(() {
+                              w = g;
+                            });
+                            print("correct");
                           }
+                          else {
+                            AssetsAudioPlayer.newPlayer().open(
+                              Audio("assets/not.ogg"),
+                            );
+                            setState(() {
+                              w = r;
+                              if (b2 == n1) {
+                                setState(() {
+                                  w2 = g2;
+                                });
+                              }
+                              else if (b3 == n1) {
+                                setState(() {
+                                  w3 = g3;
+                                });
+                              }
+                            });
+                            print("wrong");
+                          }
+                        }
 
-                        },
-                        child: Text("a)      $b1"),
+                      },
+                      child: Text("a)      $b1",
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black
+                      ),
                       ),
                     ),
-                    Container(
-                      child: TextButton(
-                        onPressed: (){
-                          if(x==1){
-                            if (b2 == n1) {
-                              AssetsAudioPlayer.newPlayer().open(
-                                Audio("assets/correct.ogg"),
-                              );
-                              print("correct");
-                            }
-                            else {
-                              AssetsAudioPlayer.newPlayer().open(
-                                Audio("assets/not.ogg"),
-                              );
-                              print("wrong");
-                            }
+                  ),
+                  SizedBox(height: 10,),
+                  Container(
+                    color: w2,
+                    child: TextButton(
+                      onPressed: (){
+                        if(x==1){
+                          if (b2 == n1) {
+                            AssetsAudioPlayer.newPlayer().open(
+                              Audio("assets/correct.ogg"),
+                            );
+                            setState(() {
+                              w2 = g2;
+                            });
+                            print("correct");
                           }
+                          else {
+                            AssetsAudioPlayer.newPlayer().open(
+                              Audio("assets/not.ogg"),
+                            );
+                            setState(() {
+                              w2 = r2;
+                              if (b3 == n1) {
+                                setState(() {
+                                  w3 = g3;
+                                });
+                              }
+                              else if (b1 == n1) {
+                                setState(() {
+                                  w = g;
+                                });
+                              }
+                            });
 
-                        },
-                        child: Text("b)      $b2"),
-                      ),
-                    ),
-                    Container(
-                      child: TextButton(
-                        onPressed: (){
-                          if(x==1) {
-                            if (b3 == n1) {
-                              AssetsAudioPlayer.newPlayer().open(
-                                Audio("assets/correct.ogg"),
-                              );
-                              print("correct");
-                            }
-                            else {
-                              AssetsAudioPlayer.newPlayer().open(
-                                Audio("assets/not.ogg"),
-                              );
-                              print("wrong");
-                            }
+                            print("wrong");
                           }
-                        },
-                        child: Text("c)      $b3"),
-                      ),
+                        }
+
+                      },
+                      child: Text("b)      $b2", style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black
+                      ),),
                     ),
-                    SizedBox(height: 20,),
-                    TextButton(onPressed: (){
-                      setState(() {
-                         x=0;
-                         n1=0;
-                       n2=0;
+                  ),
+                  SizedBox(height: 10,),
+                  Container(
+                    color: w3,
+                    child: TextButton(
+                      onPressed: (){
+                        if(x==1) {
+                          if (b3 == n1) {
+                            AssetsAudioPlayer.newPlayer().open(
+                              Audio("assets/correct.ogg"),
+                            );
+                            setState(() {
+                              w3 = g3;
+                            });
+                            print("correct");
+                          }
+                          else {
+                            AssetsAudioPlayer.newPlayer().open(
+                              Audio("assets/not.ogg"),
+                            );
+                            setState(() {
+                              w3 = r3;
+                              if (b2 == n1) {
+                                setState(() {
+                                  w2 = g2;
+                                });
+                              }
+                              else if (b1 == n1) {
+                                setState(() {
+                                  w = g;
+                                });
+                              }
+                            });
+                            print("wrong");
+
+                          }
+                        }
+                      },
+                      child: Text("c)      $b3",
+                        style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black
+                        ),),
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  TextButton(onPressed: (){
+                    setState(() {
+                      x=0;
+                      n1=0;
+                      n2=0;
                       n3=0;
-                       n4=0;
-                        b1=0;
-                       b2=0;
-                        b3=0;
-                      });
-                    }, child: Text('Reset',
+                      n4=0;
+                      b1=0;
+                      b2=0;
+                      b3=0;
+                      w=Colors.white;
+                      w2=Colors.white;
+                      w3=Colors.white;
+                    });
+                  }, child: Text('Reset',
                     style: TextStyle(
                       fontSize: 18,
                       fontWeight: FontWeight.bold,
                       color: Colors.black,
                     ),),
-                      style: ElevatedButton.styleFrom(
-                          shadowColor: Colors.green,
-                          backgroundColor: Colors.blue,
-                          shape: StadiumBorder(),
-                          padding: EdgeInsets.symmetric(horizontal: 35,vertical: 20)),
+                    style: ElevatedButton.styleFrom(
+                        shadowColor: Colors.green,
+                        backgroundColor: Colors.blue,
+                        shape: StadiumBorder(),
+                        padding: EdgeInsets.symmetric(horizontal: 35,vertical: 20)),
 
-                    ),
-                  ],
-                ),
-              )
-          ),
+                  ),
+                ],
+              ),
+            )
         ),
+      ),
     );
 
   }
