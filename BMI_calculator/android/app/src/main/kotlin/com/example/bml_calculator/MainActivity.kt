af697f25eea6f02710912719ce84b7bdbbab720e
zeeshi045 <zeeshanfarooq486@gmail.com>
2022-10-31 20:10:39 +0500

    Startup

---
+++ BMI_calculator/android/app/src/main/kotlin/com/example/bml_calculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bml_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- README.md
+++
@@ -1,4 +0,0 @@
-# MAD-subject
-Name:Muhammad Zeeshan Farooq
-Reg:sp19-bcs-045
-Section:A
