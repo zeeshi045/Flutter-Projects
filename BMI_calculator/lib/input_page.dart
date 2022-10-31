942891463ce04591e7e7d3a86e76cb140dc7553e
zeeshi045 <zeeshanfarooq486@gmail.com>
2022-10-31 21:50:06 +0500

    update
    Simple UI design using Container widget

--- BMI_calculator/lib/input_page.dart
+++ BMI_calculator/lib/input_page.dart
@@ -15,8 +15,64 @@ class _InputPageState extends State<InputPage> {
         backgroundColor: Color(0xFF0A0E21),
         title: Text('BMI_calculator'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body:Column(
+        children:[
+        Expanded(child: Row(
+          children: [
+            Expanded(
+          child:Container(
+            margin: EdgeInsets.all(15.0),
+            decoration: BoxDecoration(
+              color: Color(0xFF1D1E33),
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+
+      ),
+    ),
+            Expanded(child:Container(
+    margin: EdgeInsets.all(15.0),
+    decoration: BoxDecoration(
+    color: Color(0xFF1D1E33),
+    borderRadius: BorderRadius.circular(10.0),
+    ),
+
+    ),
+            ),
+          ],
+        ),
+
+        ),
+          Expanded(child:Container(
+            margin: EdgeInsets.all(15.0),
+            decoration: BoxDecoration(
+              color: Color(0xFF1D1E33),
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+
+          ),),
+          Expanded(child: Row(
+            children: [
+              Expanded(child:Container(
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  color: Color(0xFF1D1E33),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+
+              ),),
+              Expanded(child:Container(
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  color: Color(0xFF1D1E33),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+
+              ),),
+            ],
+          ),
+
+          ),
+        ],
       ),
 
     );
