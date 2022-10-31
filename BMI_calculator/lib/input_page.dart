031044372c920a250891de1958711f15c78526ad
zeeshi045 <zeeshanfarooq486@gmail.com>
2022-10-31 21:19:32 +0500

    Update
    1)Design Appbar (appbar ka color or name set kiya han using ThemeData).
    2)Text widget use kiya tha body ma phir usko alag dart file ma paste kiya han

---
+++ BMI_calculator/lib/input_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  const InputPage({Key? key}) : super(key: key);
+
+  @override
+  State<InputPage> createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF0A0E21),
+        title: Text('BMI_calculator'),
+      ),
+      body: Center(
+        child: Text('Body Text'),
+      ),
+
+    );
+  }
+}
