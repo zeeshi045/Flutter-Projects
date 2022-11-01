5b50f09da165279cc457122bcef605e74ab6dd24
zeeshi045 <zeeshanfarooq486@gmail.com>
2022-11-01 20:57:29 +0500

    UPDATE
    Result page ka UI desgin complete kiya han

--- BMI_calculator/lib/constant.dart
+++ BMI_calculator/lib/constant.dart
@@ -7,7 +7,27 @@ const activeColor=Color(0xFF1d1e33);
 const deactiveColor=Color(0xFF111328);
 
 
+const kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+
 const kNumberStyle=TextStyle(
   fontWeight: FontWeight.bold,
   fontSize: 25.0,
+);
+const kResultTextStyle = TextStyle(
+  color: Color(0xFF24D876),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBMITextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBodyTextStyle = TextStyle(
+  fontSize: 22.0,
 );
--- BMI_calculator/lib/result.dart
+++ BMI_calculator/lib/result.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:bml_calculator/constant.dart';
+import 'package:bml_calculator/Container.dart';
+
+import 'input_page.dart';
 class ResultScreen extends StatelessWidget {
   const ResultScreen({Key? key}) : super(key: key);
 
@@ -8,7 +12,59 @@ class ResultScreen extends StatelessWidget {
       appBar: AppBar(
         title:Text('BMI Result'),
       ),
-      body: Text('body data'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              child: Center(
+                child: Text(
+                  'Your Result',
+                  style: kTitleTextStyle,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: RepeatContainerCode(
+              colors: activeColor,
+              card: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'mm',
+                    style: kResultTextStyle,
+                  ),
+                  Text(
+                    '18.3',
+                    style: kBMITextStyle,
+                  ),
+                  Text(
+                    'ifsdfs',
+                    textAlign: TextAlign.center,
+                    style: kBodyTextStyle,
+                  ),
+                ],
+              ), onPressed: () {},
+            ),
+          ),
+          GestureDetector(
+            onTap: (){
+              Navigator.push(context,MaterialPageRoute(builder:( context)=>InputPage()));
+            },
+            child: Container(
+              child:Center(child: Text('RE_Calculate',style: kNumberStyle,)),
+              color: Color(0xFFEB1555),
+              margin: EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: 80.0,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
