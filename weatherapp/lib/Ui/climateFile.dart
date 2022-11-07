061510c2d0954c9cf9392db5c74c8b99ff9228ee
zeeshi045 <zeeshanfarooq486@gmail.com>
2022-11-07 22:54:52 +0500

    update
    detching data from api key in json

--- weatherapp/lib/Ui/climateFile.dart
+++ weatherapp/lib/Ui/climateFile.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import '../utils/apifile.dart' as util;
 class Climate extends StatefulWidget {
   const Climate({Key? key}) : super(key: key);
 
@@ -28,12 +28,12 @@ class _ClimateState extends State<Climate> {
       body: Stack(
         children: [
           Center(
-              child: Image(
-                image: AssetImage('images/umbrella.png'),
-                height: 1200.0,
-                width: 600.0,
-                fit: BoxFit.fill,
-              ),
+            child: Image(
+              image: AssetImage('images/umbrella.png'),
+              height: 1200.0,
+              width: 600.0,
+              fit: BoxFit.fill,
+            ),
           ),
           Container(
             alignment: Alignment.topRight,
@@ -47,9 +47,15 @@ class _ClimateState extends State<Climate> {
       ),
     );
   }
+    Future <Map> getWeather(String appId, String city) async {
+      String apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=$city&appid='
+          '${util.apiId}&units=imperial';
+      //https://api.openweathermap.org/data/2.5/weather?q=vehari&appid=cd444af68b389fa65c3371d707a024a1&units=metric
+    }
 }
 
-TextStyle cityStyle() {
+
+  TextStyle cityStyle() {
   return TextStyle(
       color: Colors.white,
       fontSize: 22.9,
