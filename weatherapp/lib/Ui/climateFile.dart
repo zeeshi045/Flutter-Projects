bb4dbd2c37e5c7a99148d30ce4407a18e032332e
zeeshi045 <zeeshanfarooq486@gmail.com>
2022-11-07 22:02:07 +0500

    update
    climate app Ui

---
+++ weatherapp/lib/Ui/climateFile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Climate extends StatefulWidget {
+  const Climate({Key? key}) : super(key: key);
+
+  @override
+  State<Climate> createState() => _ClimateState();
+}
+
+class _ClimateState extends State<Climate> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- weatherapp/lib/main.dart
+++ weatherapp/lib/main.dart
@@ -1,18 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:weatherapp/Screen0.dart';
-import 'package:weatherapp/Screen2.dart';
-import 'package:weatherapp/screen1.dart';
-
+import 'package:weatherapp/Ui/climateFile.dart';
 void main() {
-  runApp(MyApp());
-
-}
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Screen1(),
-    );
-  }
+  runApp(new MaterialApp(
+    title: 'Climate',
+    home: Climate(),
+  ),
+  );
 }
