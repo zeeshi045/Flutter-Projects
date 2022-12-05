1433f0be9927c67c074f2f739af0058beb5734a2
zeeshi045 <112756669+zeeshi045@users.noreply.github.com>
2022-12-05 16:23:32 +0500

    shareperference task

--- tasbee_app/lib/advance.dart
+++ tasbee_app/lib/advance.dart
@@ -1,356 +1,63 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-import 'advance.dart';
-List<Widget> tasbe=[];
-List<qu> ff=List.generate(tasbe.length, (index) => qu('${b[index]}'));
-int a=0;
-String b="";
-int c=0;
-class advance extends StatefulWidget {
-  const advance({Key? key}) : super(key: key);
-
-  @override
-  State<advance> createState() => _advanceState();
+void main(){
+  runApp(MyApp());
 }
-class _advanceState extends State<advance> {
-  final name = TextEditingController();
-  final limit = TextEditingController();
-  String   tasbheename="";
-  int  tasbheelimit=0;
+
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text('Tasbee App'),
-          backgroundColor: Colors.black87,
-        ),
-        body: Container(
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-
-          gradient: LinearGradient(
-          begin: Alignment.centerRight,
-          end: Alignment.bottomLeft,
-          colors: [
-            Colors.amber,
-            Colors.black87,
-          ],
-          ),
-    ),
-          child:ListView.builder(itemCount: tasbe.length, itemBuilder: (context,index){
-            return Card(
-              child: ListTile(
-                title: Column(
-                  children: [
-                    Column(
-                      children:
-                        tasbe,
-
-                    ),
-                    Text(ff[index].name),
-                  ],
-                ),
-                leading: SizedBox(
-                  width: 50,
-                  height: 50,
-                ),
-                onTap: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=>Screen(gg:ff[index],)));
-                },
-              ),
-            );
-          },
-
-          )
-        ),
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.transparent,
-        child: TextButton(
-          onPressed: () {
-            AlertDialog alert = AlertDialog(
-              title:Column(
-                  children: [
-                  Text('Enter Tasbe name'),
-              TextField(
-                controller:  name,
-                decoration: InputDecoration(
-                  hintText: "Astaghfirullah",
-                  border: OutlineInputBorder(),
-                  suffixIcon: IconButton(
-                      onPressed:(){
-                        name.clear();
-                      },
-                      icon: const Icon(Icons.clear_rounded)
-                  ),
-
-                ),
-
-
-              ),
-
-
-              Text('Enter Tasbe Limit'),
-              TextField(
-
-                controller:  limit,
-                decoration: InputDecoration(
-                  hintText: "10000",
-                  border: OutlineInputBorder(),
-                  suffixIcon: IconButton(
-                      onPressed:(){
-                        limit.clear();
-                      },
-                      icon: const Icon(Icons.clear_rounded)
-                  ),
-                ),
-              ),
-
-              ],
-            ),
-
-              actions: [
-                TextButton(
-                  onPressed: (){
-                    tasbheelimit=int.parse(limit.text);
-                    tasbheename=name.text;
-                    b=tasbheename;
-                    a=tasbheelimit;
-                    setState(() {
-                      b=tasbheename;
-                      a=tasbheelimit;
-                      c=0;
-                    });
-                    tasbe.add(ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.black, //background color of button
-                          side: BorderSide(width:3, color:Colors.amberAccent), //border width and color
-                          elevation: 3, //elevation of button
-                          shape: RoundedRectangleBorder( //to set border radius to button
-                              borderRadius: BorderRadius.circular(30)
-                          ),
-                          //content padding inside button
-                        ),
-                        onPressed: (){
-
-                          if (!mounted) return;
-                          setState(() {
-                            b=tasbheename;
-                            c=10;
-                            print("tasbheekaro");
-                          });
-
-                        },
-                        child: Row(
-                          children: [
-                            Text(tasbheename),
-                            Text(tasbheelimit.toString()),
-                          ],
-                        )));
-                    name.clear();
-                    limit.clear();
-                    Navigator.push(context,MaterialPageRoute(builder: (context)=>advance()));
-                  },
-                  child: Text('Add',),
-                ),
-
-              ],
-            );
-            showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return alert;
-              },
-            );
-          },
-          child: Container(
-            child:Center(child: Text('Custom',)),
-            color: Color(0xFFEB1555),
-            margin: EdgeInsets.only(top: 10.0),
-            width: double.infinity,
-            height: 80.0,
-          ),
-        ),
-        elevation: 0,
-      ),
+    return MaterialApp(
+      home: Home(),
     );
   }
 }
-class qu{
-  String name="";
-  String n="";
- qu(this.name);
-}
 
-class Screen extends StatefulWidget {
-  final qu gg;
-  const Screen({Key? key,required this.gg,}) : super(key: key);
-
+class Home extends StatefulWidget{
   @override
-  State<Screen> createState() => _ScreenState();
+  _HomeState createState() => _HomeState();
 }
 
-class _ScreenState extends State<Screen> {
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-return Scaffold(
-appBar: AppBar(
-title:Text('Tasbeeh App',style: TextStyle(
-fontWeight: FontWeight.bold,
-fontSize: 30),
+    return  Scaffold(
+        body: Container(
+            padding: EdgeInsets.only(top:80, left:20, right:20),
+            child: Wrap(
+              alignment: WrapAlignment.start,
+              children: [
 
-),
-
-centerTitle: true,
-backgroundColor: Colors.amberAccent,
-),
-body: Container(
-child: Column(
-
-children: [
-Container(
-height: 150,
-color: Colors.black,
-child: Column(
-
-children: [
-Center(
-child: Text("$b", style: TextStyle(
-color: Colors.amber,
-fontWeight: FontWeight.bold,
-fontStyle: FontStyle.italic,
-fontFamily: 'Open Sans',
-fontSize: 30),),),
-Center(
-child: Text("$c", style: TextStyle(
-color: Colors.amber,
-fontWeight: FontWeight.bold,
-fontStyle: FontStyle.italic,
-fontFamily: 'Open Sans',
-fontSize: 20),),),
-SizedBox(
-height: 15,
-
-),
-
-Row(
-mainAxisAlignment: MainAxisAlignment.center,
-
-children: [
-SizedBox(
-height: 35,
-width: 100,
-child: SizedBox(
-height:50, //height of button
-width:300, //width of button
-child:TextButton(
-style: TextButton.styleFrom(
-//background color of button
-side: BorderSide(width:3, color:Colors.amberAccent), //border width and color
-
-shape: RoundedRectangleBorder( //to set border radius to button
-borderRadius: BorderRadius.circular(30)
-),
-//content padding inside button
-),
-onPressed: (){
-if(c==b){
-AlertDialog alert = AlertDialog(
-title: Text("Success"),
-content: Text("Limit Reached :$b"),
-actions: [
-TextButton(
-onPressed: (){
-Navigator.of(context).pop();
-},
-child: Text('ok',),
-),
-
-],
-);
-showDialog(
-context: context,
-builder: (BuildContext context) {
-return alert;
-},
-);
-}
-else{
-setState(() {
-c++;
-});
-}
-
-},
-child: Text("Count",style: TextStyle(
-color: Colors.amber,
-fontWeight: FontWeight.bold,
-fontStyle: FontStyle.italic,
-fontFamily: 'Open Sans',
-fontSize: 12),),)
-),
-),
-SizedBox(width: 15,),
-SizedBox(
-height: 35,
-width: 100,
-child: SizedBox(
-height:50, //height of button
-width:300, //width of button
-child:ElevatedButton(
-style: ElevatedButton.styleFrom(
-primary: Colors.black, //background color of button
-side: BorderSide(width:3, color:Colors.amberAccent), //border width and color
-elevation: 3, //elevation of button
-shape: RoundedRectangleBorder( //to set border radius to button
-borderRadius: BorderRadius.circular(30)
-),
-//content padding inside button
-),
-onPressed: (){
-setState(() {
-c=0;
-});
-
-//code to execute when this button is pressed.
-},
-child: Text("Reset",style: TextStyle(
-color: Colors.amber,
-fontWeight: FontWeight.bold,
-fontStyle: FontStyle.italic,
-fontFamily: 'Open Sans',
-fontSize: 12),),)
-)
-),
-],
-),
+                ElevatedButton(
+                    onPressed: () async {
+                      SharedPreferences pre = await SharedPreferences.getInstance();
+                      pre.setString("name", "FlutterCampus"); //save string
+                      pre.setInt("age", 25); //save integer
+                      pre.setBool("married", false); //save boolean
+                      pre.setDouble("price", 240.50); //save double
+                      pre.setStringList("tags", ["Flutter", "Dart", "App"]); //save List
+                    },
+                    child: Text("Save Data")
+                ),
 
 
+                ElevatedButton(
+                    onPressed: () async {
+                      SharedPreferences pre = await SharedPreferences.getInstance();
+                      String name = pre.getString("name") ?? "";
+                      //here "??" is a fallback operator,
+                      //if the return is null, it will be the assigned value;
 
-],
-)
-),
-
-
-
-
-Container(
-width: MediaQuery.of(context).size.width,
-    height:510,
-    decoration: BoxDecoration(
-    image: DecorationImage(
-    fit: BoxFit.fill,
-    image: AssetImage("assets/11.jpg"),
-    ),
-    ),
-    )
-
-    ],
-    ),
-
-    ),
+                      int age = pre.getInt("age") ?? 0;
+                      bool married = pre.getBool("married") ?? false;
+                      double price = pre.getDouble("price") ?? 0.00;
+                      List<String> tags = pre.getStringList("tags") ?? [];
+                    },
+                    child: Text("Get Data")
+                )
+              ],)
+        )
     );
   }
 }
